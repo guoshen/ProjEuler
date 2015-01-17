9b3b8cea0bf8ef4654def230aff9400c167ac613
guoshen2 <tonyguo526@hotmail.com>
2015-01-17 14:43:23 -0500

    V.1.0

---
+++ Q22/utils.h
@@ -0,0 +1,9 @@
+ #ifndef UTILS_H
+#define UTILS_H
+
+
+void quicksort_strs(char *args[], unsigned int len);
+char ** split(char str[], char * delim, int * num );
+void freeArray(char ** strarr, int length);
+
+#endif
