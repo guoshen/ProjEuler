7c7dd54d34e3517fa34b7c81849c6783952c2eb2
guoshen2 <tonyguo526@hotmail.com>
2015-01-20 23:21:00 -0500

    edited comments

--- Q31/q31.cpp
+++ Q31/q31.cpp
@@ -14,11 +14,11 @@ use smaller coins.
 Dynamic Programming:
 
 
-D[200, {200,100,25,10,5,1}]= D[200, {100,50,20,5,2,1}]+D[0,{100,50,20,5,2,1}];
+D[200, {200,100,50,20,10,5,2,1}]= D[200,{100,50,20,10,5,2,1}]+D[0,{100,50,20,10,5,2,1}];
 
-Number of ways to make 200p using {200,100,25,10,5,1} coins is the sum of:
-the number of ways to make 200p using {100,50,20,5,2,1} coins using 0 200p coins
-and the number of ways to make 200p using {100,50,20,5,2,1} coins using 1 200p coin
+Number of ways to make 200p using {200,100,50,20,10,5,2,1} coins is the sum of:
+the number of ways to make 200p using {100,50,20,10,5,2,1} coins using 0 200p coins
+and the number of ways to make 200p using {100,50,20,10,5,2,1} coins using 1 200p coin
 
 Countinue  recursive pattern until base case(s):
 D[200,{1}]=1;
@@ -38,10 +38,7 @@ int main()
 	// eight types of coins
 	int coins[8]={200,100,50,20,10,5,2,1};
 
-	cout<<"Number of ways to make 200p:"<<makeCurr(200, coins,8);
-
-
-
+	cout<<"Number of ways to make 200p:"<<makeCurr(200, coins, 8);
 }
 
 int makeCurrHelper(int amount, int coins[], int numCoins, int currCoin)
@@ -59,8 +56,6 @@ int makeCurrHelper(int amount, int coins[], int numCoins, int currCoin)
         return 0;
 
 	}
-
-
         int count=0;
 
         int i;
@@ -72,20 +67,14 @@ int makeCurrHelper(int amount, int coins[], int numCoins, int currCoin)
             count+=makeCurrHelper(amount-i*coins[currCoin],coins,numCoins,currCoin+1);
         }
         return count;
-
-
 }
 
 //returns the number of ways to make amount using the denominations of coins array
 //assume coins array is sorted from greatest to least
 int makeCurr(int amount, int coins[],int numCoins)
 {
-
 	int currCoin=0;
-
 	return makeCurrHelper(amount,coins,numCoins,currCoin);
-
-
 }
 
 
